f9bc9d6f2726a8b09bcd58ffa860f4fdcccaf3ee
kwonkyo-gim <hamaboyk@naver.com>
2023-04-11 13:30:03 +0900

    20230411_01
    C# 교과서 마스터하기 01. C#
    C# 강의 - C# 교과서 마스터하기 02. 준비사항
    C# 강의 - C# 교과서 마스터하기 03. Hello, World
    C# 강의 - C# 교과서 마스터하기 04. C# 기본 문법 이해하기

--- DotNet/DotNet/Program.cs
+++ DotNet/DotNet/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorldCoreWithVisoualStudio
+namespace DotNet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World.");
-
         }
     }
 }
