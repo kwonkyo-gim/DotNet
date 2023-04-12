babfa905fd19197197e8c34b7558d7dfb1e1d15a
kwonkyo-gim <hamaboyk@naver.com>
2023-04-12 11:34:35 +0900

    20230412_05
    C# 교과서 마스터하기 15. 프로그래밍 공통 for 문 사용 데모

---
+++ DotNet/DotNet/DotNet/15_For/ForFactorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class ForFactorial
+{
+    static void Main()
+    {
+        int fact = 0;
+
+        for (int i = 1; i <= 4; i++)
+        {
+            Console.Write($"{i}! -> ");
+            fact = 1;
+            
+            for (int j = 1; j <= i; j++)
+            {
+                fact = fact * j; 
+            }
+            Console.WriteLine($"{fact,2}");
+        }
+    }
+}
