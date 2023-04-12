425d9b643612ed52057d115a6150dd41e09802a8
kwonkyo-gim <hamaboyk@naver.com>
2023-04-12 11:55:48 +0900

    20230412_06
    C# 교과서 마스터하기 16. 프로그래밍 공통 for 문 사용 데모

---
+++ DotNet/DotNet/DotNet/16_While/ForEach.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ForEach
+{
+    static void Main()
+    {
+        string str = "ABC123";
+
+        foreach (char c in str)
+        {
+            Console.Write($"{c}\t");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ DotNet/DotNet/DotNet/16_While/ForEachUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ForEachUp
+{
+    static void Main()
+    {
+        var str = "abc123";
+
+        foreach (var c in str)   //var 변수
+        {
+            Console.Write($"{c}\t");
+        }
+        Console.WriteLine();
+    }
+}
