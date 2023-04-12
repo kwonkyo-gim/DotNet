a3994257b800632fb0a3ea32b07692709defe24b
kwonkyo-gim <hamaboyk@naver.com>
2023-04-12 13:16:56 +0900

    20230412_07
    C# 교과서 마스터하기 17. break, continue, goto로 반복문 제어하기

---
+++ DotNet/DotNet/DotNet/17_BreakContinueGoto/BreakFor.cs
@@ -0,0 +1,13 @@
+﻿class BreakFor
+{
+    static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (i >= 0)
+            {
+                break; 
+            }
+        } 
+    }
+}
---
+++ DotNet/DotNet/DotNet/17_BreakContinueGoto/ContinueDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ContinueDemo
+{
+    static void Main()
+    {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                continue;  
+            }
+            sum += i;
+        }
+        Console.WriteLine("SUM: {0}", sum); 
+    }
+}
