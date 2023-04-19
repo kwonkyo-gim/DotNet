f031574b9b9b3b045ea99d1f15a0cf173b6826b7
kwonkyo-gim <hamaboyk@naver.com>
2023-04-19 16:28:01 +0900

    20230419_09
    C# 교과서 마스터하기 49. 확장 메서드 만들기 요약 설명

---
+++ DotNet/DotNet/DotNet/49_ExtensionMethod/ExtensionFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+static class ExtensionFunction
+{
+    
+    static string Three(this string value)
+    {
+      
+        return value.Substring(0, 3);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("안녕하세요.".Three());
+    }
+}
