19ce3c5c843d415277a3e97f0cf6cf1b77e8ebf6
kwonkyo-gim <hamaboyk@naver.com>
2023-04-20 14:28:12 +0900

    20230420_06
    C# 교과서 마스터하기 56. 비동기 프로그래밍
    C# 교과서 마스터하기 57. 인메모리 데이터베이스 프로그래밍 맛보기

---
+++ DotNet/DotNet/DotNet/57_InMemory/RepositoryPatternDemo.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+
+public interface ITableRepository
+{
+    string GetAll();
+}
+
+public class TableInMemoryRepository : ITableRepository
+{
+    public string GetAll()
+    {
+        return "인-메모리 데이터베이스 사용";
+    }
+}
+
+public class TableSqlRepository : ITableRepository
+{
+    public string GetAll() => "SQL Server 데이터베이스 사용";
+}
+
+public class TableXmlRepository : ITableRepository
+{
+    public string GetAll() => "XML 데이터베이스 사용";
+}
+
+class RepositoryPatternDemo
+{
+    static void Main()
+    {
+      
+        string repo = "SQL";
+
+        ITableRepository repository;
+        if (repo == "InMemoy")
+        {
+            repository = new TableInMemoryRepository();
+        }
+        else if (repo == "XML")
+        {
+            repository = new TableXmlRepository();
+        }
+        else
+        {
+            repository = new TableSqlRepository();
+        }
+
+        Console.WriteLine(repository.GetAll());
+    }
+}
---
+++ DotNet/DotNet/DotNet/57_InMemory/SignBaseSignRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class SignBase
+{
+    public int SignId { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+
+public class SignRepository
+{
+    public List<SignBase> GetAll()
+    {
+        var signs = new List<SignBase>()
+        {
+            new SignBase() { SignId = 1, Email = "a@a.com", Password = "1234" },
+            new SignBase() { SignId = 2, Email = "b@b.com", Password = "2345" },
+            new SignBase() { SignId = 3, Email = "c@c.com", Password = "3456" },
+        };
+
+        return signs;
+    }
+}
+
+
+public class SignContext
+{
+    public List<SignBase> Signs
+    {
+        get
+        {
+            return (new SignRepository()).GetAll();
+        }
+    }
+}
+
+
+class SignBaseSignRepository
+{
+    static void Main()
+    {
+        var signs = (new SignContext()).Signs;
+
+        foreach (var sign in signs)
+        {
+            Console.WriteLine($"{sign.SignId}, {sign.Email}, {sign.Password}");
+        }
+    }
+}
