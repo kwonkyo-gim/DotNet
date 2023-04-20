3df805035aafe9dadf07a2415fb1cbd4d4ee48d0
kwonkyo-gim <hamaboyk@naver.com>
2023-04-20 11:42:26 +0900

    20230420_05
    C# 교과서 마스터하기 55. 스레드(Thread)는 작업자 한 명

---
+++ DotNet/DotNet/DotNet/55_Thread/ConcurrencyFor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+class ConcurrencyFor
+{
+    static void Main()
+    {
+        for (int i = 0; i < 200_000; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
