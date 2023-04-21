ee050eeeb613e9a56ba48c14b7c4fd5ec2834457
kwonkyo-gim <hamaboyk@naver.com>
2023-04-21 15:29:40 +0900

    20230421_03
    C# WPF 강의 #1 프로젝트만들기

---
+++ DotNet/DotNet/DotNet/62_Modern/PatternMatchingSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PatternMatchingSwitch
+{
+    class Shape { }
+
+    class Rectangle : Shape { }
+
+    class Circle : Shape { }
+
+    class PatternMatchingSwitch
+    {
+        static void Main()
+        {
+            ShowShape(new Circle());        
+            ShowShape(new Rectangle());     
+        }
+
+        static void ShowShape(Shape shape)
+        {
+            switch (shape)
+            {
+                case Circle s:
+                    Console.WriteLine("원");
+                    break;
+                case Rectangle r:
+                    Console.WriteLine("사각형");
+                    break;
+            }
+        }
+    }
+}
