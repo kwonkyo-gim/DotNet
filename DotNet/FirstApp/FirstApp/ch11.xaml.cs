02c3ef8833d646c6fcf2ce89012affbbf9f1edeb
kwonkyo-gim <hamaboyk@naver.com>
2023-04-24 15:10:52 +0900

    20230424_09
    C# WPF 강좌 #11 슬라이더 Slider 바인딩

--- DotNet/FirstApp/FirstApp/ch11.xaml.cs
+++ DotNet/FirstApp/FirstApp/ch11.xaml.cs
@@ -15,11 +15,11 @@ using System.Windows.Shapes;
 namespace FirstApp
 {
     /// <summary>
-    /// ch6.xaml에 대한 상호 작용 논리
+    /// ch11.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class ch6 : Window
+    public partial class ch11 : Window
     {
-        public ch6()
+        public ch11()
         {
             InitializeComponent();
         }
